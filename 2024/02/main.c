1c62ccf1b37b3a0e3f760cb6360e4666e15b00ad
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2024-12-11 23:44:03 +0000

    I give up

--- 2024/02/main.c
+++ 2024/02/main.c
@@ -3,26 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 #define REPORT_COUNT 1000
 #define LONGEST_REPORT_COUNT 23
 #define LONGEST_REPORT_CHAR_COUNT 23
-
-typedef struct ReportSafetyState
-{
-	bool is_safe;
-	int bad_index_left;
-	int bad_index_right;
-} ReportSafetyState;
-
-void print_report(const int* report, const int report_len)
-{
-	for (int i = 0; i < report_len; ++i)
-	{
-		printf("%d ", report[i]);
-	}
-	printf("\n");
-}
 
 void copy_array_excluding_index(int* dest, const int* src, const int src_len, const int index)
 {
@@ -32,7 +17,7 @@ void copy_array_excluding_index(int* dest, const int* src, const int src_len, co
 	memcpy(dest + index, src + index + 1, sizeof(int) * (src_len - (index + 1)));
 }
 
-ReportSafetyState is_report_safe(const int* report, const int report_len)
+bool is_report_safe(const int* report, const int report_len)
 {
 	assert(report_len >= 2);
 
@@ -79,30 +64,26 @@ ReportSafetyState is_report_safe(const int* report, const int report_len)
 		++i_right;
 	}
 
-	return is_safe 
-		? (ReportSafetyState){ true, -1, -1 } 
-		: (ReportSafetyState){ false, i_left, i_right };
+	return is_safe;
 }
 
 bool is_report_safe_with_dampener(const int* report, const int report_len)
 {
-	printf("\n");
-	print_report(report, report_len);
 	const int damp_report_len = report_len - 1;
 	int* damp_report = malloc(sizeof(int) * damp_report_len);
 	
+	assert(damp_report);
+
 	int i = 0;
 	bool is_safe = false;
 
 	while (!is_safe && i < report_len)
 	{
 		copy_array_excluding_index(damp_report, report, report_len, i);
-		printf("%d: ", i);
-		print_report(damp_report, damp_report_len);
-		is_safe = is_report_safe(damp_report, damp_report_len).is_safe;
+		usleep(1); // WTF WHY DO I NEED AN ARBITRARY CALL TO ANY METHOD FOR THE OUTPUT TO BE CORRECT
+		is_safe = is_report_safe(damp_report, damp_report_len);
 		++i;
 	}
-	printf("%s\n", is_safe ? "safe" : "not safe");
 
 	free(damp_report);
 	return is_safe;
@@ -112,13 +93,16 @@ int main()
 {
 	FILE* file = fopen("input.txt", "r");
 
-	assert(file != NULL);
+	assert(file);
 
 	// data + \n + \0
 	const int current_line_len = LONGEST_REPORT_CHAR_COUNT + 2;
 	const int current_line_size = sizeof(char) * current_line_len;
 	char* current_line = malloc(current_line_size);
+	assert(current_line);
+
 	int* current_report = malloc(sizeof(int) * LONGEST_REPORT_COUNT);
+	assert(current_report);
 
 	int safe_reports_without_dampener = 0;
 	int safe_reports_with_dampener = 0;
@@ -139,18 +123,18 @@ int main()
 			current_token_ptr = strtok(NULL, " ");
 		}
 
-		ReportSafetyState state = is_report_safe(current_report, current_report_len);
+		bool is_safe = is_report_safe(current_report, current_report_len);
 
-		safe_reports_without_dampener += state.is_safe;
-		safe_reports_with_dampener += state.is_safe  || is_report_safe_with_dampener(current_report, current_report_len);
+		safe_reports_without_dampener += is_safe;
+		safe_reports_with_dampener += is_safe || is_report_safe_with_dampener(current_report, current_report_len);
 	}
 	
-	printf("Part 1 = %d\n", safe_reports_without_dampener);
-	printf("Part 2 = %d\n", safe_reports_with_dampener);
-
 	free(current_report);
 	free(current_line);
 	fclose(file);
+
+	printf("Part 1 = %d\n", safe_reports_without_dampener);
+	printf("Part 2 = %d\n", safe_reports_with_dampener);
 
 	return 0;
 }
