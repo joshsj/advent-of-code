70cd96a7846c93adbef52968fbf28c0aa1eea9de
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2024-12-11 23:07:30 +0000

    Done part 2 but something weird is happening

--- 2024/02/main.c
+++ 2024/02/main.c
@@ -8,37 +8,104 @@
 #define LONGEST_REPORT_COUNT 23
 #define LONGEST_REPORT_CHAR_COUNT 23
 
-bool is_report_safe(const int* report, const int len)
+typedef struct ReportSafetyState
 {
-	assert(len >= 2);
+	bool is_safe;
+	int bad_index_left;
+	int bad_index_right;
+} ReportSafetyState;
 
-	int prev = report[0];
-	int curr = report[1];
-	bool is_increasing = curr > prev;
-	int i = 1;
-
-	for(;;)
+void print_report(const int* report, const int report_len)
+{
+	for (int i = 0; i < report_len; ++i)
 	{
-		int diff = curr - prev;
+		printf("%d ", report[i]);
+	}
+	printf("\n");
+}
 
-		if (abs(diff) < 1
-			|| abs(diff) > 3
-			|| (is_increasing && diff < 0)
-			|| (!is_increasing && diff > 0))
+void copy_array_excluding_index(int* dest, const int* src, const int src_len, const int index)
+{
+	assert(index < src_len);
+
+	memcpy(dest, src, sizeof(int) * index);
+	memcpy(dest + index, src + index + 1, sizeof(int) * (src_len - (index + 1)));
+}
+
+ReportSafetyState is_report_safe(const int* report, const int report_len)
+{
+	assert(report_len >= 2);
+
+	bool report_increasing; 
+	bool report_increasing_init;
+
+	int i_left = 0;
+	int i_right = 1;
+	bool is_safe = true;
+
+	while (i_right < report_len && is_safe)
+	{
+		int l = report[i_left];
+		int r = report[i_right];
+
+		int diff = r - l;
+		int abs_diff = abs(diff);
+
+		if (abs_diff < 1 || abs_diff > 3)
 		{
-			return false;
+			is_safe = false;
+			break;
 		}
 
-		++i;
+		bool pair_increasing = diff > 0;
 
-		if (i == len)
+		if (!report_increasing_init) 
 		{
-			return true;
+			report_increasing = pair_increasing;
+			report_increasing_init = true;
+
+			i_left = i_right;
+			++i_right;
+			continue;
 		}
 		
-		prev = curr;
-		curr = report[i];
+		if (pair_increasing != report_increasing)
+		{
+			is_safe = false;
+			break;
+		}
+
+		i_left = i_right;
+		++i_right;
 	}
+
+	return is_safe 
+		? (ReportSafetyState){ true, -1, -1 } 
+		: (ReportSafetyState){ false, i_left, i_right };
+}
+
+bool is_report_safe_with_dampener(const int* report, const int report_len)
+{
+	printf("\n");
+	print_report(report, report_len);
+	const int damp_report_len = report_len - 1;
+	int* damp_report = malloc(sizeof(int) * damp_report_len);
+	
+	int i = 0;
+	bool is_safe = false;
+
+	while (!is_safe && i < report_len)
+	{
+		copy_array_excluding_index(damp_report, report, report_len, i);
+		printf("%d: ", i);
+		print_report(damp_report, damp_report_len);
+		is_safe = is_report_safe(damp_report, damp_report_len).is_safe;
+		++i;
+	}
+	printf("%s\n", is_safe ? "safe" : "not safe");
+
+	free(damp_report);
+	return is_safe;
 }
 
 int main()
@@ -53,7 +120,8 @@ int main()
 	char* current_line = malloc(current_line_size);
 	int* current_report = malloc(sizeof(int) * LONGEST_REPORT_COUNT);
 
-	int safe_reports = 0;
+	int safe_reports_without_dampener = 0;
+	int safe_reports_with_dampener = 0;
 	
 	for (int i = 0; i < REPORT_COUNT; ++i)
 	{
@@ -71,10 +139,14 @@ int main()
 			current_token_ptr = strtok(NULL, " ");
 		}
 
-		safe_reports += is_report_safe(current_report, current_report_len);
+		ReportSafetyState state = is_report_safe(current_report, current_report_len);
+
+		safe_reports_without_dampener += state.is_safe;
+		safe_reports_with_dampener += state.is_safe  || is_report_safe_with_dampener(current_report, current_report_len);
 	}
 	
-	printf("Part 1 = %d\n", safe_reports);
+	printf("Part 1 = %d\n", safe_reports_without_dampener);
+	printf("Part 2 = %d\n", safe_reports_with_dampener);
 
 	free(current_report);
 	free(current_line);
